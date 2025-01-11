69d47b5af94457411434bba68bb4d28b4ac82352
Bryan Xavier Paredes Loyola <xavier.paredesloyola@gmail.com>
2025-01-11 09:03:47 -0500

    Avances movies

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    
+    MOVIE_GENRES = [
+        ('Action', 'Accion'),
+        ('SciFi', 'Ciencia Ficcion'),
+        ('Comedy', 'Comedia'),
+        ('Documental', 'Documental'),
+        ('Drama', 'Drama'),
+        ('Fantasy', 'Fantasia'),
+    ]
+    genre = models.CharField(max_length=30, null=False, choices=MOVIE_GENRES)
+    movie_director = models.CharField(max_length=50, null=False)
+    publication_year = models.DateField()
+    sinopsis = models.TextField(max_length=500)
+    picture = models.ImageField(upload_to='movie_images')
+    
+    def __str__(self):
+        return f'{self.title} - {self.publication_year}'
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(id = movie_id)
+    template = loader.get_template('display_movies.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
